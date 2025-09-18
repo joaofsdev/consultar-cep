d8c47601579ac067f811cf23011d8189f3bbc13f
joaofsdev <joao.franciscos047@gmail.com>
2025-09-17 21:44:58 -0300

    atualiza: modernizacao do sistema de dark e white mode

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
 function alterarModo() {
-  let btn = document.getElementById("modo");
-  let corpo = document.body;
-  let titulo = document.getElementById("titulo");
-  let secao = document.getElementsByTagName("section")[0];
-  let rodape = document.getElementsByTagName("footer")[0];
-
-  if (btn.innerText == "Modo Escuro") {
+  const btn = document.getElementById("modo");
+  const body = document.body;
+  
+  body.classList.toggle("dark-mode");
+  
+  if (body.classList.contains("dark-mode")) {
     btn.innerText = "Modo Claro";
-    corpo.style.background = "rgb(32, 32, 32)";
-    corpo.style.color = "white";
-    titulo.style.color = "white";
-    secao.style.background = "rgb(32, 32, 32)";
-    secao.style.color = "white";
-    secao.style.border = "2px solid white";
-    rodape.style.color = "white";
   } else {
     btn.innerText = "Modo Escuro";
-    corpo.style.background = "rgb(250, 249, 249)";
-    corpo.style.color = "black";
-    titulo.style.color = "black";
-    secao.style.background = "rgb(255, 255, 255)";
-    secao.style.color = "black";
-    secao.style.border = "2px solid black";
-    rodape.style.color = "black";
   }
 }
 
